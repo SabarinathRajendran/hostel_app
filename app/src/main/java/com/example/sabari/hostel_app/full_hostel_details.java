08de2aa27ffbbcdbd05084693d0f5fb7dd8a6840
sab <sabarinathkrajenderan@gmail.com>
2018-08-30 22:07:21 +0530

    next

---
+++ app/src/main/java/com/example/sabari/hostel_app/full_hostel_details.java
@@ -0,0 +1,13 @@
+package com.example.sabari.hostel_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class full_hostel_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_hostel_details);
+    }
+}
